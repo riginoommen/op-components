6ba3dc62b16cf43ee19c4cac034fe65dcb9d0e2d
Rigin Oommen <riginoommen@gmail.com>
2021-01-19 13:35:58 +0530

    Feedback Component

--- packages/opc-feedback/src/opc-feedback.ts
+++ packages/opc-feedback/src/opc-feedback.ts
@@ -9,10 +9,10 @@
  * @author Rigin Oommen
  *
  * Created at     : 2021-01-18 14:53:24 
- * Last modified  : 2021-01-18 23:41:08
+ * Last modified  : 2021-01-19 13:35:27
  */
 
-import { LitElement, html, property, customElement, internalProperty } from 'lit-element';
+import { LitElement, html, property, customElement, internalProperty, query } from 'lit-element';
 import style from './opc-feedback.scss';
 import { repeat } from 'lit-html/directives/repeat.js';
 
@@ -21,8 +21,7 @@ export class OpcFeedback extends LitElement {
   @property({ type: String, attribute: 'url' }) url = '/feedback';
   @internalProperty() openFeedbackModal = false;
   @internalProperty() openConfirmationModal = false;
-  @internalProperty() summary = '';
-  @internalProperty() description = '';
+  @internalProperty() protected summary = '';
   @internalProperty() experience = '';
   @internalProperty() category = '';
   @internalProperty() experienceList = [{
@@ -53,6 +52,9 @@ export class OpcFeedback extends LitElement {
   {
     name: 'Other',
   }];
+
+  @query('textarea') textarea: HTMLTextAreaElement;
+  
   static get styles() {
     return [style];
   }
@@ -61,21 +63,12 @@ export class OpcFeedback extends LitElement {
     this.summary = '';
     this.experience = '';
     this.category = '';
-    this.description = '';
     this.error = '';
+    this.textarea.value = '';
   }
 
   submitFeedback() {
     this.category = (this.experience !== 'Sad') ? 'FEEDBACK' : 'BUG';
-    this.description = `
-${(this.error) ? `Error: ${this.error}` : ''}
-
-Browser Information
-____________________
-Stack - ${navigator.appVersion}
-URL - ${window.location.href}
-Relative Path - ${window.location.pathname}
-`;
     this.dispatchEvent(new CustomEvent('opc-feedback:emit', {
       detail: {
         message: 'Emitted the Feedback',
@@ -83,12 +76,17 @@ Relative Path - ${window.location.pathname}
           summary: this.summary,
           experience: this.experience,
           category: this.category,
-          description: this.description
+          description: {
+            "stack": navigator.appVersion,
+            "path": window.location.pathname,
+            "error": (this.error) ? `Error: ${this.error}` : ''
+          }
         }
       }
     }));
     this.resetForm();
   }
+  
   render() {
     return html`
     <link type="text/css" rel="stylesheet" href="https://unpkg.com/@patternfly/patternfly/patternfly.css"
@@ -100,12 +98,12 @@ Relative Path - ${window.location.pathname}
         <header>
           <h3 class="op-feedback__title">How was your overall experience?.</h3>
         </header>
-        <p class="feedback-subtitle pf-u-text-align-center pf-u-font-size-sm pf-u-pt-md pf-u-pb-md">It will help us to
+        <p class="op-feedback__subtitle pf-u-text-align-center pf-u-font-size-sm pf-u-pt-md pf-u-pb-md">It will help us to
           improve platform</p>
         ${repeat(this.experienceList, (experience) => html`
         <div
-          class="pf-c-chip pf-m-draggable feedback-chip ${this.experience === experience.name ? 'feedback-chip-active' : ''}"
-          @click="${e => this.experience = experience.name}" tabindex="0">
+          class="pf-c-chip pf-m-draggable op-feedback-chip ${this.experience === experience.name ? 'op-feedback-chip__active' : ''}"
+          @click="${e => this.experience = experience.name}" @keydown=${e => (e.key === 'Enter')?  this.experience = experience.name :''} tabindex="0">
           <span class="pf-c-chip__icon">
             <img src="${experience.assetUrl}" alt="${experience.name} icon" width="20px">
           </span>
@@ -113,38 +111,35 @@ Relative Path - ${window.location.pathname}
         </div>
         `)}
         <hr class="pf-c-divider pf-u-pt-md pf-u-pb-md ${this.experience !== 'Sad' ? 'pf-u-display-none-on-sm' : ''}" />
-        <p class="feedback-subtitle pf-u-font-size-md ${this.experience !== 'Sad' ? 'pf-u-display-none-on-sm' : ''}">What is
+        <p class="op-feedback__subtitle pf-u-font-size-md ${this.experience !== 'Sad' ? 'pf-u-display-none-on-sm' : ''}">What is
           wrong?</p>
         ${repeat(this.errorList, (error) => html`
         <div
-          class="pf-c-chip pf-m-draggable feedback-chip ${this.experience !== 'Sad' ? 'pf-u-display-none-on-sm' : ''} ${this.error === error.name ? 'feedback-chip-active' : ''}"
-          @click="${e => this.error = error.name}" tabindex="0">
+          class="pf-c-chip pf-m-draggable op-feedback-chip ${this.experience !== 'Sad' ? 'pf-u-display-none-on-sm' : ''} ${this.error === error.name ? 'feedback-chip-active' : ''}"
+          @click="${e => this.error = error.name}" @keydown=${e => (e.key === 'Enter')?  this.error = error.name :''} tabindex="0">
           <span class="pf-c-chip__text">${error.name}</span>
         </div>
         `)}
-        <p class="feedback-subtitle pf-u-font-size-md pf-u-pt-md">Summary</p>
-        <textarea id="summary" rows="3" name="summary" @change="${e => {
-          this.summary = e.srcElement.value;
-          e.srcElement.value ='';
-        }}"
-          placeholder="How can we do better?" class="pf-c-form-control pf-m-resize-vertical" required></textarea>
-        <button class="pf-c-button pf-m-block" type="button" @click="${e => {
+        <p class="op-feedback__subtitle pf-u-font-size-md pf-u-pt-md">Summary</p>
+        <textarea id="summary" rows="3" name="summary" @input="${(e: HTMLElementEventMap | any) => this.summary = e.target.value}" placeholder="How can we do better?" class="pf-c-form-control pf-m-resize-vertical" required></textarea>
+        <button class="pf-c-button pf-m-block ${this.experience.length===0 ? 'pf-u-display-none-on-sm' : ''}" type="button" @click="${e => {
             this.submitFeedback();
             this.openFeedbackModal = !this.openFeedbackModal;
             this.openConfirmationModal = !this.openConfirmationModal;
           }}">Submit</button>
+          <button class="pf-c-button pf-m-block ${this.experience.length!==0 ? 'pf-u-display-none-on-sm' : ''}" type="button" disabled>Submit</button>
       </form>
     </dialog>
     <dialog id="op-feedback__panel" class="op-feedback__panel" .open="${this.openConfirmationModal}">
       <h2 class="pf-u-text-align-center">Thanks your experience is important to us!</h2>
-      <p class="feedback-subtitle pf-u-text-align-center pf-u-font-size-xs pf-u-pb-md">Each time a friend submits a
+      <p class="op-feedback__subtitle pf-u-text-align-center pf-u-font-size-xs pf-u-pb-md">Each time a friend submits a
         experience, it creates a task for our developer team to resolve it with priority.</p>
       <hr class="pf-c-divider" />
       <a href="${this.url}" target="_blank">
         <p class="pf-u-text-align-center pf-u-p-xs">View Feedback</p>
       </a>
       <hr class="pf-c-divider" />
-      <p class="feedback-subtitle pf-u-text-align-center pf-u-p-xs pointer" @click="${e => this.openConfirmationModal = false}">
+      <p class="op-feedback__subtitle pf-u-text-align-center pf-u-p-xs" @click="${e => this.openConfirmationModal = false}" @keydown=${e => (e.key === 'Enter')? this.openConfirmationModal = false :'' } tabindex="0">
         close</p>
     </dialog>
     <button id="op-feedback__button" type="button" class="op-feedback__button" @click="${e => {
